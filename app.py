d69c38e2a1b7acd55899a4770076587c3e955c5a
Keenan Graham <ksgraham@stanford.edu>
2023-12-06 16:30:04 -0800

    First cluster

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ from aws_cdk import CfnOutput
 from aws_cdk import CustomResource
 from aws_cdk import Duration
 
+from aws_cdk.aws_eks import Cluster
+from aws_cdk.aws_eks import KubernetesVersion
+
 from constructs import Construct
 
 from shared_infrastructure.cherry_lab.environments import US_WEST_2
@@ -20,11 +23,46 @@ class KubernetesStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        cluster = Cluster(
+            self,
+            'Cluster',
+            version=KubernetesVersion.V1_27,
+            kubectl_layer=KubectlV27Layer(
+                self,
+                'kubectl',
+            )
+        )
+
+        cluster.add_manifest(
+            'testpod',
+            {
+                'apiVersion': 'v1',
+                'kind': 'Pod',
+                'metadata': {
+                    'name': 'testpod',
+                },
+                'spec': {
+                    'containers': [
+                        {
+                            'name': 'testcontainer',
+                            'image': 'paulbouwer/hello-kubernetes:1.5',
+                            'ports': [
+                                {
+                                    'container_port': 8080
+                                }
+                            ]
+                        },
+                    ]
+                }
+            },
+        )
+
 
 KubernetesStack(
     app,
     'KubernetesStack',
     env=US_WEST_2,
 )
+
 
 app.synth()
