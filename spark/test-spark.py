b84f6f24b1dc1e04fa5f8530f178c489b2276d32
Keenan Graham <ksgraham@stanford.edu>
2025-01-30 22:24:11 -0800

    Try local bucket

--- spark/test-spark.py
+++ spark/test-spark.py
@@ -22,8 +22,8 @@ def main():
     row_count = df.count()
 
     print(f"Number of rows in the DataFrame: {row_count}")
-
-    df = spark.read.text("s3a://igvf-files-dev-logs/")
+    df = spark.read.text("s3a://encode-build-log/2017-03-25*")
+#    df = spark.read.text("s3a://igvf-files-dev-logs/")
 #    df.persist()
     df.count()
 
