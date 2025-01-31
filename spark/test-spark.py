d632a5985ca1de5684acfff693c76d34f573347a
Keenan Graham <ksgraham@stanford.edu>
2025-01-30 23:10:42 -0800

    update

--- spark/test-spark.py
+++ spark/test-spark.py
@@ -9,7 +9,7 @@ def main():
         .getOrCreate()
 
     data = [
-        Row(name="Klalice", age=30),
+        Row(name="eKlalice", age=30),
         Row(name="Bob", age=25),
         Row(name="Charlie", age=35),
         Row(name="David", age=40)
@@ -22,8 +22,8 @@ def main():
     row_count = df.count()
 
     print(f"Number of rows in the DataFrame: {row_count}")
-    df = spark.read.text("s3a://encode-imputation-logs/")
-#    df = spark.read.text("s3a://igvf-files-dev-logs/")
+   # df = spark.read.text("s3a://encode-imputation-logs/")
+    df = spark.read.text("s3a://igvf-files-dev-logs/")
 #    df.persist()
     print('Number of logs', df.count())
 
