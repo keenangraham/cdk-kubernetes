ea110633f3fbee33e3ffeb56fff4c3f3c1213e3a
Otto Jolanki <ojolanki@stanford.edu>
2025-04-28 13:19:36 -0700

    try using spark app directly from yaml. customize executor sa in dag definition

---
+++ dags/test_spark_app_file.py
@@ -0,0 +1,41 @@
+from datetime import datetime, timedelta
+from pathlib import Path
+
+from airflow import DAG
+from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
+
+
+default_args = {
+    'owner': 'me',
+    'depends_on_past': False,
+    'start_date': datetime(2025, 2, 4),
+    'retries': 1,
+    'schedule': None,
+    'retry_delay': timedelta(minutes=5),
+}
+
+# Define path to your Spark application YAML
+# This assumes the DAG file is in a structure like airflow/dags/ and the YAML is in the repo
+# REPO_ROOT = Path(__file__).parents[2]  # Adjust based on your folder structure
+# SPARK_APP_YAML_PATH = str(REPO_ROOT / "k8s/spark-apps/test-spark-app.yaml")
+
+
+with DAG(
+    dag_id='test-spark-app-file',
+    default_args=default_args,
+    description='A DAG to run Spark on Kubernetes',
+    schedule_interval=timedelta(days=1),
+    catchup=False,
+) as dag:
+
+    # Use the application_file parameter to directly point to the YAML file
+    spark_task = SparkKubernetesOperator(
+        task_id='spark_task_from_file',
+        namespace='data-stack-dev',
+        application_file='k8s/spark-apps/test-spark-app.yaml',
+        executor_config={
+            'KubernetesExecutor': {
+                'service_account': 'spark-bucket-read-sa'
+            }
+        }
+    )
