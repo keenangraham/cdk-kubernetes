26287380592b65e79a3fa87c71aa51477431ca1e
Keenan Graham <ksgraham@stanford.edu>
2026-06-02 14:48:20 -0700

    Add IGVF log parsing

---
+++ dags/parse-igvf-logs.py
@@ -0,0 +1,26 @@
+from datetime import timedelta
+
+from airflow.sdk import dag
+from airflow.sdk import task
+
+from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
+
+
+@dag(
+    schedule='@daily',
+    start_date=datetime(2026, 5, 1),
+    catchup=False,
+)
+def parse_igvf_logs():
+
+    @task
+    def launch_spark_job():
+        spark_task = SparkKubernetesOperator(
+            task_id='parse-igvf-logs',
+            namespace='data-stack-dev',
+            application_file='spark-apps/parse-igvf-logs.yaml',
+        )
+
+    launch_spark_job()
+
+parse_igvf_logs()
