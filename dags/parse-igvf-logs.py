30b356574fbec9f3b4c1a359c719a1ce015b05c6
Keenan Graham <ksgraham@stanford.edu>
2026-06-02 15:11:08 -0700

    Fix dag

--- dags/parse-igvf-logs.py
+++ dags/parse-igvf-logs.py
@@ -2,7 +2,6 @@ from datetime import datetime
 from pendulum import timezone
 
 from airflow.sdk import dag
-from airflow.sdk import task
 
 from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
 
@@ -13,15 +12,10 @@ from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKu
     catchup=False,
 )
 def parse_igvf_logs():
-
-    @task
-    def launch_spark_job():
-        spark_task = SparkKubernetesOperator(
-            task_id='parse-igvf-logs',
-            namespace='data-stack-dev',
-            application_file='spark-apps/parse-igvf-logs.yaml',
-        )
-
-    launch_spark_job()
+    SparkKubernetesOperator(
+        task_id='parse-igvf-logs',
+        namespace='data-stack-dev',
+        application_file='spark-apps/parse-igvf-logs.yaml',
+    )
 
 parse_igvf_logs()
