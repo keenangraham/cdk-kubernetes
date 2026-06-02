0712f2cd000968ccc851d4821bbdd878a6bc7f50
Keenan Graham <ksgraham@stanford.edu>
2026-06-02 16:45:55 -0700

    Update interval

--- dags/parse-igvf-logs.py
+++ dags/parse-igvf-logs.py
@@ -2,12 +2,16 @@ from datetime import datetime
 from pendulum import timezone
 
 from airflow.sdk import dag
+from airflow.timetables.simple import CronDataIntervalTimetable
 
 from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
 
 
 @dag(
-    schedule='0 10 * * *',
+    timetable=CronDataIntervalTimetable(
+        '0 10 * * *',
+        timezone='America/Los_Angeles',
+    ),
     start_date=datetime(2026, 5, 1, tzinfo=timezone("America/Los_Angeles")),
     catchup=False,
 )
