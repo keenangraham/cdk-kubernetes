7b191fac58d3bd39e8d0e2140de18ffae985d4d1
Keenan Graham <ksgraham@stanford.edu>
2026-06-23 16:42:45 -0700

    Try different glob

--- spark/parse-encode-logs.py
+++ spark/parse-encode-logs.py
@@ -1,23 +1,55 @@
 import os
-from datetime import date as date_type
+import boto3
 
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, regexp_extract
 
 
+def get_s3a_paths(s3_client, bucket: str, prefix: str) -> list:
+    s3a_paths = []
+    continuation_token = None
+
+    while True:
+        list_kwargs = {'Bucket': bucket, 'Prefix': prefix}
+        if continuation_token:
+            list_kwargs['ContinuationToken'] = continuation_token
+
+        response = s3_client.list_objects_v2(**list_kwargs)
+
+        if 'Contents' in response:
+            for obj in response['Contents']:
+                s3a_paths.append(f"s3a://{bucket}/{obj['Key']}")
+
+        if response.get('NextContinuationToken'):
+            continuation_token = response['NextContinuationToken']
+        else:
+            break
+
+    return s3a_paths
+
+
 def main():
     spark = SparkSession.builder \
         .appName("S3 Encode Log Reader") \
+        .config("spark.hadoop.mapreduce.input.fileinputformat.split.minsize", "134217728") \
+        .config("spark.hadoop.mapreduce.input.fileinputformat.split.maxsize", "268435456") \
         .getOrCreate()
 
     log_regex_pattern = r'([^ ]*) ([^ ]*) \[(.*?)\] ([^ ]*) ([^ ]*) ([^ ]*) ([^ ]*) ([^ ]*) ("[^"]+"|-) (\d+|-|-) ([^ ]*) (\d+|-|-) (\d+|-|-) (\d+|-|-) (\d+|-|-) ("[^"]*"|-) ("[^"]*"|-) ([^ ]*)'
+
+    session = boto3.Session(
+        aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
+        aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
+    )
+    s3_client = session.client('s3')
 
     date = os.environ['DATE']
     print("*****************")
     print('DATE IS:' + date)
     print("*****************")
 
-    df = spark.read.text(f's3a://encode-public-logs/{date}*')
+    files_to_read = get_s3a_paths(s3_client, 'encode-public-logs', date)
+    df = spark.read.text(files_to_read)
 
     parsed_df = df.select(
         regexp_extract(col('value'), log_regex_pattern, 1).alias('bucket_owner'),
@@ -40,7 +72,7 @@ def main():
         regexp_extract(col('value'), log_regex_pattern, 18).alias('version_id'),
     )
     output_path = f's3a://encode-parsed-logs/parquet/date={date}'
-    parsed_df.repartition(4).write.mode('overwrite').parquet(output_path)
+    parsed_df.repartition(30).write.mode('overwrite').parquet(output_path)
 
 
 if __name__ == "__main__":
